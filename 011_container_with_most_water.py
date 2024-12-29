f1a159d86645830001008339bd98ca3c716c0641
Revanth Singothu <revanthsingothu@gmail.com>
2024-12-29 15:26:58 +0530

    29-12-2024

---
+++ 011_container_with_most_water.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        l, r = 0, len(height) - 1
+
+        maxArea = 0
+        while l != r:
+            area = (r - l) * min(height[l], height[r])
+            if area > maxArea:
+                maxArea = area
+            if height[l] > height[r]:
+                r -= 1
+            else:
+                l += 1
+        
+        return maxArea
