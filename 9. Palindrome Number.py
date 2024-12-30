0205bcd1928983f96c31b3a9c0b22a295b49e04b
Revanth Singothu <revanthsingothu@gmail.com>
2024-12-30 21:00:40 +0530

    30 dec 24

---
+++ 9. Palindrome Number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        strx = str(x)
+        xrev = strx[::-1]
+        if strx == xrev:
+            return True
+        
+        return False
